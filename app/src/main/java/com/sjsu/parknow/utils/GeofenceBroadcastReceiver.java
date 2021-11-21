6bc9c579a09d85304db73d91bdbf819ba94d1677
yadnyshree <yadnyshree@gmail.com>
2021-11-20 23:01:35 -0800

    bug fixes

--- app/src/main/java/com/sjsu/parknow/utils/GeofenceBroadcastReceiver.java
+++ app/src/main/java/com/sjsu/parknow/utils/GeofenceBroadcastReceiver.java
@@ -1,14 +1,17 @@
 package com.sjsu.parknow.utils;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingEvent;
+import com.google.android.gms.maps.model.LatLng;
 import com.sjsu.parknow.NearbyFragment;
 
 import java.util.List;
@@ -17,35 +20,26 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
     private static final String TAG = "GeofenceBroadcastReceiv";
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        // Toast.makeText(context,"Geofence triggered...",Toast.LENGTH_SHORT).show();
-       // String message = intent.getStringExtra("Yes");
-        //if("Yes".equals(message)) {
 
-            //testFn();
-      //  }
-       /*if(intent.getIdentifier().equals("26")){
-            Toast.makeText(context, "Yes clicked", Toast.LENGTH_SHORT).show();
-        } else if(intent.getIdentifier().equals("7")){
-            Toast.makeText(context, "No clicked", Toast.LENGTH_SHORT).show();
-        }*/
         NotificationHelper notificationHelper = new NotificationHelper(context);
-
+        Bundle bundle = intent.getExtras();
+        String id = String.valueOf(bundle.get("ID"));
+        double lat = (double) bundle.get("Lat");
+        double longitude = (double) bundle.get("Long");
+        getGeoCooridnnates(notificationHelper,lat,longitude);
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
 
         if (geofencingEvent.hasError()) {
             Log.d(TAG, "onReceive: Error receiving geofence event...");
             return;
         }
-        List<Geofence> geofenceList = geofencingEvent.getTriggeringGeofences();
+ /*       List<Geofence> geofenceList = geofencingEvent.getTriggeringGeofences();
         for (Geofence geofence: geofenceList) {
             Log.d(TAG, "onReceive: " + geofence.getRequestId());
-        }
+        }*/
         Location location = geofencingEvent.getTriggeringLocation();
         int transitionType = geofencingEvent.getGeofenceTransition();
-        notificationHelper.setLatitude(location.getLatitude());
-        notificationHelper.setLongitude(location.getLongitude());
+
         switch (transitionType) {
 
 
@@ -64,4 +58,15 @@ public class GeofenceBroadcastReceiver extends BroadcastReceiver {
                 break;
         }
     }
+
+
+    public void getGeoCooridnnates( NotificationHelper notificationHelper, double lat, double longitude){
+        notificationHelper.setLatitude(lat);
+        notificationHelper.setLongitude(longitude);
+
+    }
+
+
+
+
 }
